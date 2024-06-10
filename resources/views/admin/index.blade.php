4436a8ece8956b78f676b8b984ed91882176405b
muhamad-tanahmerdeka7 <muhay990@gmail.com>
2024-06-09 18:53:11 -0700

    admin page

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -1,22 +1,21 @@
 <!DOCTYPE html>
-<html lang="en">
+<html>
 
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    @include('admin.css')
 </head>
 
 <body>
+    @include('admin.header')
+    <div class="d-flex align-items-stretch">
+        <!-- Sidebar Navigation-->
+        @include('admin.sidebar')
+        <!-- Sidebar Navigation end-->
 
-    Admin
-    <x-app-layout>
 
+        @include('admin.body')
 
-    </x-app-layout>
-
-
+        @include('admin.footer')
 </body>
 
 </html>
